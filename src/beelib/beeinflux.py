b4e06bd0d2f08c56ac513c8540f62a826dc1e7fa
David Blanco Bautista <73123058+Davidoff2103@users.noreply.github.com>
2025-07-24 17:16:46 +0200

    Add docstrings and comments to beeinflux.py

--- src/beelib/beeinflux.py
+++ src/beelib/beeinflux.py
@@ -6,6 +6,15 @@ import pandas as pd
 
 
 def connect_influx(influx_connection):
+    """
+    Connect to InfluxDB.
+
+    Args:
+        influx_connection (dict): Configuration for connecting to InfluxDB.
+
+    Returns:
+        influxdb_client.InfluxDBClient: Connected InfluxDB client.
+    """
     client = influxdb_client.InfluxDBClient(
         url=influx_connection['connection']['url'],
         org=influx_connection['connection']['org'],
@@ -16,22 +25,47 @@ def connect_influx(influx_connection):
 
 
 def run_query(influx_connection, query):
+    """
+    Run a query on InfluxDB.
+
+    Args:
+        influx_connection (dict): Configuration for connecting to InfluxDB.
+        query (str): Query string.
+
+    Returns:
+        pandas.DataFrame: Result of the query.
+    """
     client = connect_influx(influx_connection)
     query_api = client.query_api()
     return query_api.query_data_frame(query)
 
 
 def get_timeseries_by_hash(d_hash, freq, influx_connection, ts_ini, ts_end):
+    """
+    Get timeseries data from InfluxDB by hash.
+
+    Args:
+        d_hash (str): Hash identifier for the timeseries.
+        freq (str): Frequency string for time series (e.g., '1h').
+        influx_connection (dict): Configuration for connecting to InfluxDB.
+        ts_ini (datetime): Start timestamp.
+        ts_end (datetime): End timestamp.
+
+    Returns:
+        pandas.DataFrame: Timeseries data in DataFrame format.
+    """
     aggregation_window = int(isodate.parse_duration(freq).total_seconds() * 10**9)
     start = int(ts_ini.timestamp()) * 10**9
     end = int(ts_end.timestamp()) * 10**9
-    query = f"""
-        from(bucket: "{influx_connection['bucket']}")
+    query = f"""from(bucket: "{influx_connection['bucket']}")
         |> range(start: time(v:{start}), stop: time(v:{int(end)}))
-        |> filter(fn: (r) => r["_measurement"] == "{influx_connection['measurement']}")
-        |> filter(fn: (r) => r["hash"] == "{d_hash}")
-        |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-        |> filter(fn: (r) => r["is_null"]==0.0)
+        |> filter(fn: (r) =>
+            r["_measurement"] == "{influx_connection['measurement']}")
+        |> filter(fn: (r) =>
+            r["hash"] == "{d_hash}")
+        |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value")
+        |> filter(fn: (r) =>
+            r["is_null"]==0.0)
         |> keep(columns: ["_time", "value", "end", "isReal"])
     """
     client = connect_influx(influx_connection)
@@ -44,4 +78,3 @@ def get_timeseries_by_hash(d_hash, freq, influx_connection, ts_ini, ts_end):
     df = df[["start", "end", "isReal", "value"]]
     df.set_index("start", inplace=True)
     return df
-
