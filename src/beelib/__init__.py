b1d1799259154b9dc08178f87778590c01ab0ebd
David Blanco Bautista <73123058+Davidoff2103@users.noreply.github.com>
2025-07-24 17:13:47 +0200

    Add docstrings and comments to __init__.py

--- src/beelib/__init__.py
+++ src/beelib/__init__.py
@@ -6,3 +6,8 @@ from . import beetransformation
 from . import beedruid
 from . import beerdf
 from . import beeinflux
+
+__all__ = ["beecfg", "beehbase", "beekafka", "beesecurity", "beetransformation", "beedruid", "beerdf", "beeinflux"]
+
+# This file is the package entry point for beelib
+# It imports all submodules to make them available in the package namespace
