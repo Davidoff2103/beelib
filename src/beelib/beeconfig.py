566611612641b5b6940c9df01c54a24b1e81219c
David Blanco Bautista <73123058+Davidoff2103@users.noreply.github.com>
2025-07-24 17:14:27 +0200

    Add docstrings and comments to beeconfig.py

--- src/beelib/beeconfig.py
+++ src/beelib/beeconfig.py
@@ -4,11 +4,24 @@ import re
 
 
 def read_config(conf_file=None):
+    """
+    Read configuration from a file or environment variable.
+
+    Args:
+        conf_file (str, optional): Path to the configuration file. If not provided,
+            the configuration will be read from the environment variable CONF_FILE.
+
+    Returns:
+        dict: Configuration dictionary with processed values.
+    """
     if conf_file:
         conf = json.load(open(conf_file))
     else:
         conf = json.load(open(os.environ['CONF_FILE']))
+    
+    # Process Neo4j configuration
     for k in [re.match(r'neo4j.*', k).string for k in conf.keys() if re.match(r'neo4j.*', k) is not None]:
         if 'auth' in conf[k]:
             conf[k]['auth'] = tuple(conf[k]['auth'])
+    
     return conf
